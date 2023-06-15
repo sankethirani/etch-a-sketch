b96a25432187dfaf9743ca054d08898185ff263c
Sanket <sankethirani.sh@gmail.com>
2023-06-15 14:45:42 +1200

    feat: add hover effect

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 function populateGrid(){
-    const gridContainer = document.querySelector('#grid-container');
+    const gridContainer = document.querySelector("#grid-container");
     for (i = 0; i < 16; i++){
-        console.log(i);
-        let divRow = document.createElement('div');
+        let divRow = document.createElement("div");
         gridContainer.appendChild(divRow);
         for (j = 0; j < 16; j++){
-            const div = document.createElement('div');
+            const div = document.createElement("div");
+            div.className = "cell";
             divRow.appendChild(div);
         }
     }
 }
 
+function hoverEffect(e){
+    this.style.backgroundColor = "red";
+}
+
 populateGrid();
+cells = document.querySelectorAll(".cell");
+cells.forEach(cell => cell.addEventListener("pointerenter", hoverEffect));
