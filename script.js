c42146a873bf711c19e8c792fb8cf7587e6e107e
Sanket <sankethirani.sh@gmail.com>
2023-07-10 09:56:08 +1200

    Feat: Add darkening effect

--- script.js
+++ script.js
@@ -17,10 +17,6 @@ function populateGrid(cellCount){
     } 
 }
 
-function hoverEffect(e){
-    this.style.backgroundColor = "red";
-}
-
 function updateGrid(){
     const input = prompt("How many rows and columns do you want on the grid?");
     const cellCount = parseInt(input);
@@ -32,15 +28,12 @@ function updateGrid(){
     }
 }
 
-function activateHoverEffect(){
-    cells.forEach(cell => cell.addEventListener("pointerenter", hoverEffect));
-}
-
-function activateRainbowEffect(){
-    cells.forEach(cell => cell.addEventListener("pointerenter", rainbowEffect));
+function hoverEffect(e){
+    this.style.backgroundColor = "red";
 }
 
 function rainbowEffect(e){
+    this.style.filter = null;
     const rgb = [];
     for (i = 0; i < 3; i++){
         rgb[i] = Math.floor(Math.random() * 255);
@@ -48,9 +41,44 @@ function rainbowEffect(e){
     this.style.backgroundColor = `rgb(${rgb.toString()})`;
 }
 
+function darkeningEffect(e){
+    let currentBrightness = this.style.filter;
+    if (currentBrightness === ""){
+        this.style.backgroundColor = "white";
+        this.style.filter = "brightness(1)";
+    }
+    else{
+        let brightnessValue = currentBrightness.substring(currentBrightness.indexOf("(") + 1, currentBrightness.lastIndexOf(")"));
+        brightnessValue -= 0.1;
+        if (!(brightnessValue < 0)){
+            this.style.filter = `brightness(${brightnessValue})`;
+        } 
+    }
+    
+}
+
+function activateDarkeningEffect(){
+    cells.forEach(cell => clearEventListener(cell));
+    cells.forEach(cell => cell.addEventListener("pointerenter", darkeningEffect));
+}
+function activateHoverEffect(){
+    cells.forEach(cell => cell.addEventListener("pointerenter", hoverEffect));
+}
+
+function activateRainbowEffect(){
+    cells.forEach(cell => clearEventListener(cell));
+    cells.forEach(cell => cell.addEventListener("pointerenter", rainbowEffect));
+}
+
+function clearEventListener(cell){
+    cell.replaceWith(cell.cloneNode(true));
+    cells = document.querySelectorAll(".cell");
+}
+
 populateGrid(16);
 let cells = document.querySelectorAll(".cell");
 activateHoverEffect();
 document.querySelector("#btn-update-grid").addEventListener("click", updateGrid);
 document.querySelector("#btn-rainbow").addEventListener("click", activateRainbowEffect);
+document.querySelector("#btn-darkening").addEventListener("click", activateDarkeningEffect);
 
